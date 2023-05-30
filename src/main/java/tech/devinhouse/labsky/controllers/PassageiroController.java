ee48aecb9483af5f6bc32229c5237c6588de61ed
halangbacca <halanbacca@outlook.com>
2023-05-30 11:25:55 -0300

    Implementa listagem de passageiros

--- src/main/java/tech/devinhouse/labsky/controllers/PassageiroController.java
+++ src/main/java/tech/devinhouse/labsky/controllers/PassageiroController.java
@@ -1,9 +1,25 @@
 package tech.devinhouse.labsky.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.devinhouse.labsky.models.Passageiro;
+import tech.devinhouse.labsky.services.PassageiroService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/passageiros")
 public class PassageiroController {
+
+    @Autowired
+    private PassageiroService service;
+
+    @GetMapping
+    public List<Passageiro> listaPassageiros() {
+        return service.listaPassageiros();
+    }
+    
 }
--- src/main/java/tech/devinhouse/labsky/services/PassageiroService.java
+++ src/main/java/tech/devinhouse/labsky/services/PassageiroService.java
@@ -1,7 +1,21 @@
 package tech.devinhouse.labsky.services;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.devinhouse.labsky.models.Passageiro;
+import tech.devinhouse.labsky.repositories.PassageiroRepository;
+
+import java.util.List;
 
 @Service
 public class PassageiroService {
+
+    @Autowired
+    private PassageiroRepository repository;
+
+    public List<Passageiro> listaPassageiros() {
+        return repository.findAll();
+    }
+    
 }
